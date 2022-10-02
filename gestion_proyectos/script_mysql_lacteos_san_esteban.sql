dab757b32911d782a7ab4ef238fd92cdb6b36644
robert-virtual <robertocastill63@gmail.com>
2022-10-01 19:27:33 -0600

    prueba 2 seguridad informatica

---
+++ gestion_proyectos/script_mysql_lacteos_san_esteban.sql
@@ -0,0 +1,71 @@
+CREATE TABLE `CuentasPorPagar` (
+  `servicioProducto` varchar(50),
+  `registradoPor` varchar(50),
+  `fecha` datetime,
+  `cantidad` int,
+  `unidadDeMedida` varchar(10),
+  `proveedor` varchar(50),
+  `monto` float
+);
+
+CREATE TABLE `CuentasPorCobrar` (
+  `servicioProducto` varchar(50),
+  `registradoPor` varchar(50),
+  `fecha` datetime,
+  `cantidad` int,
+  `cliente` varchar(50),
+  `monto` float
+);
+
+CREATE TABLE `Queso` (
+  `registradoPor` varchar(255),
+  `fecha` datetime,
+  `librasProducidas` float,
+  `tipo` varchar(255),
+  `lecheEntera` int,
+  `lecheDescremada` int,
+  `sal` float,
+  `cuajo` int,
+  `chileJalapeno` int,
+  `chileBolson` int
+);
+
+CREATE TABLE `Quesillo` (
+  `registradoPor` varchar(255),
+  `fecha` datetime,
+  `librasProducidas` float,
+  `tipo` varchar(255),
+  `lecheEntera` int,
+  `lecheDescremada` int,
+  `sal` float,
+  `cuajo` int
+);
+
+CREATE TABLE `Mantequilla` (
+  `registradoPor` varchar(50),
+  `fecha` datetime,
+  `librasProducidas` float,
+  `tipo` varchar(50),
+  `lecheEntera` int,
+  `sal` float
+);
+
+CREATE TABLE `Requeson` (
+  `registradoPor` varchar(255),
+  `fecha` datetime,
+  `librasProducidas` float,
+  `lecheEntera` int,
+  `lecheDescremada` int,
+  `sal` float,
+  `cuajo` int
+);
+
+CREATE TABLE `Cuajada` (
+  `registradoPor` varchar(50),
+  `fecha` datetime,
+  `librasProducidas` float,
+  `lecheEntera` int,
+  `lecheDescremada` int,
+  `sal` float,
+  `cuajo` int
+);
